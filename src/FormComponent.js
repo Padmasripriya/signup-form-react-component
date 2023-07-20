d45d6b08fb8ede31a151bf44fe84cca83eeb89ca
Padmasripriya <padmasripriya17@gmail.com>
2023-07-20 16:49:15 -0400

    adding form validations

--- src/FormComponent.js
+++ src/FormComponent.js
@@ -1,16 +1,32 @@
 import React from "react";
+import { useState } from "react";
 
 export function FormComponent() {
+    const [error, setError] = useState('');
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    function handleValidation() {
+        if(firstName.length == 0 && lastName.length == 0 && email.length == 0 && password.length == 0){
+            setError(true)
+        }
+    }
     return(
         <div>
             <p className="signup-form-offer-desktop"><strong>Try it for free 7 days</strong> then $20/mo. thereafter</p>
             <div className="signup-form">
             <form>
-                <p><input type="text" placeholder="First Name" /></p>
-                <p><input type="text" placeholder="Last Name" /></p>
-                <p><input type="email" placeholder="Email Address" /></p>
-                <p><input type="password" placeholder="Password" /></p>
-                <button className="signup-form-button">CLAIM YOUR FREE TRAIL</button>
+            <p><input onChange={(e) => setFirstName(e.target.value)} type="text" placeholder="First Name" /></p>
+                {error && <span className="error">First Name cannot be empty</span>}
+                <p><input onChange={(e) => setLastName(e.target.value)} type="text" placeholder="Last Name" /></p>
+                {error && <span className="error">Last Name cannot be empty</span>}
+                <p><input onChange={(e) => setEmail(e.target.value)} type="email" placeholder="Email Address" /></p>
+                {error && <span className="error">Looks like this is not an email</span>}
+                <p><input onChange={(e) => setPassword(e.target.value)} type="password" placeholder="Password" /></p>
+                {error || <span className="error">Password cannot be empty</span>}
+                <button onClick={handleValidation} className="signup-form-button">CLAIM YOUR FREE TRAIL</button>
             </form>
             <p className="signup-form-footer">By clicking the button, you are agreeing to our <span style={{ color: 'hsl(0, 100%, 74%)', fontWeight: 700 }}>Terms and Services</span></p>
             </div>
