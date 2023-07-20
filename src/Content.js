a6cd3f826ce837d5bc03139d042f3165e4f7d3c2
Padmasripriya <padmasripriya17@gmail.com>
2023-07-20 13:58:48 -0400

    adding mobile style changes

--- src/Content.js
+++ src/Content.js
@@ -3,12 +3,12 @@ import React from "react";
 export function Content() {
     return(
         <div className="signup-form-content">
-            <h3>Learn to code by watching others</h3>
-            <p>
+            <h3 className="signup-form-title">Learn to code by watching others</h3>
+            <p className="signup-form-subtitle">
                 See how experienced developers solve problems in real-time. 
                 Watching scripted tutorials is great, but understanding how developers think is invaluable.
             </p>
-            <p>Try it for free 7 days then $20/mo. thereafter</p>
+            <p className="signup-form-offer"><strong>Try it for free 7 days</strong> then $20/mo. thereafter</p>
         </div>
 
     );
--- src/FormComponent.js
+++ src/FormComponent.js
@@ -4,13 +4,13 @@ export function FormComponent() {
     return(
         <div className="signup-form">
             <form>
-                <p><input type="text" placeholder="First Name" /></p>
-                <p><input type="text" placeholder="Last Name"/></p>
-                <p><input type="email" placeholder="Email Address"/></p>
-                <p><input type="password" placeholder="Password"/></p>
-                <button>CLAIM YOUR FREE TRAIL</button>
+                <p><input type="text" placeholder="First Name" required/></p>
+                <p><input type="text" placeholder="Last Name" required/></p>
+                <p><input type="email" placeholder="Email Address" required/></p>
+                <p><input type="password" placeholder="Password" required/></p>
+                <button className="signup-form-button">CLAIM YOUR FREE TRAIL</button>
             </form>
-            <p className="signup-form-footer">By clicking the button, you are agreeing to our Terms and Services</p>
+            <p className="signup-form-footer">By clicking the button, you are agreeing to our <span style={{ color: 'hsl(0, 100%, 74%)', fontWeight: 700 }}>Terms and Services</span></p>
         </div>
     );
 }
