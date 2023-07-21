d0fdaad41bbc4cff9404d8d9e6013c66dbd539c1
Padmasripriya <padmasripriya17@gmail.com>
2023-07-21 12:52:03 -0400

    added icon error icon

--- src/FormComponent.js
+++ src/FormComponent.js
@@ -17,16 +17,16 @@ export function FormComponent() {
         <div>
             <p className="signup-form-offer-desktop"><strong>Try it for free 7 days</strong> then $20/mo. thereafter</p>
             <div className="signup-form">
-            <form>
+            <form action={handleValidation}>
             <p><input onChange={(e) => setFirstName(e.target.value)} type="text" placeholder="First Name" /></p>
-                {error && <span className="error">First Name cannot be empty</span>}
+                {error || <span className="error">First Name cannot be empty</span>}       
                 <p><input onChange={(e) => setLastName(e.target.value)} type="text" placeholder="Last Name" /></p>
-                {error && <span className="error">Last Name cannot be empty</span>}
+                {error || <span className="error">Last Name cannot be empty</span>}
                 <p><input onChange={(e) => setEmail(e.target.value)} type="email" placeholder="Email Address" /></p>
-                {error && <span className="error">Looks like this is not an email</span>}
+                {error || <span className="error">Looks like this is not an email</span>}
                 <p><input onChange={(e) => setPassword(e.target.value)} type="password" placeholder="Password" /></p>
                 {error || <span className="error">Password cannot be empty</span>}
-                <button onClick={handleValidation} className="signup-form-button">CLAIM YOUR FREE TRAIL</button>
+                <button className="signup-form-button">CLAIM YOUR FREE TRAIL</button>
             </form>
             <p className="signup-form-footer">By clicking the button, you are agreeing to our <span style={{ color: 'hsl(0, 100%, 74%)', fontWeight: 700 }}>Terms and Services</span></p>
             </div>
